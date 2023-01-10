1ed542c87b93574a437f29677c274b9ea2908a2a
felix-hauger <felix.hauger@laplateforme.io>
2023-01-10 10:39:29 +0100

    added setters & getters for Card & Game classes

--- class/Card.php
+++ class/Card.php
@@ -5,14 +5,45 @@ class Card
     /**
      * @var integer used to determine cards order
      */
-    private $index;
+    private $_index;
     /**
      * @var string used to determine card image / pair
      */
-    private $name;
+    private $_name;
     /**
      * @var string use to determine card status
      * values: verso, recto or paired
      */
-    private $status = 'verso';
+    private $_status = 'verso';
+
+
+    public function getIndex()
+    {
+        return $this->_index;
+    }
+
+    public function setIndex($index)
+    {
+        $this->_index = $index;
+    }
+
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+    public function getStatus()
+    {
+        return $this->_status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->_status = $status;
+    }
 }
--- class/Game.php
+++ class/Game.php
@@ -5,15 +5,46 @@ class Game
     /**
      * @var integer determine the number of pairs
      */
-    private $pairs_nb = 6;
-    
+    private $_pairs_nb = 6;
+
     /**
      * @var boolean determine if a game is ongoing
      */
-    private $status;
-    
+    private $_status;
+
     /**
      * @var array contain cards
      */
-    private Array $cards;
+    private array $_cards;
+
+
+    public function getPairsNb()
+    {
+        return $this->_pairs_nb;
+    }
+
+    public function setPairsNb($pairs_nb)
+    {
+        $this->_pairs_nb = $pairs_nb;
+    }
+
+    public function getStatus()
+    {
+        return $this->_status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->_status = $status;
+    }
+
+    public function getCards()
+    {
+        return $this->_cards;
+    }
+
+    public function setCards($cards)
+    {
+        $this->_cards = $cards;
+    }
 }
