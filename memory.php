017fecf24ef7b0db5dacfef7a51e30a1056fcad9
felix-hauger <felix.hauger@laplateforme.io>
2023-01-11 16:00:51 +0100

    added memory.php to execute script & class + img & css file

---
+++ memory.php
@@ -0,0 +1,52 @@
+<?php
+
+// imagepng(imagecreatefromstring(file_get_contents('media/img/6.webp')), 'media/img/6.png');
+require_once 'class/Card.php';
+require_once 'class/Game.php';
+session_start();
+
+
+
+if (isset($_SESSION['game'])) {
+    if ($_SESSION['game']->getStatus() === true) {
+        $current_game = $_SESSION['game'];
+    }
+} else {
+    $game = new Game();
+
+    $game->setPairsNb(4);
+    $game->setStatus(true);
+    $game->createBoard();
+    $game->start();
+}
+// session_destroy();
+var_dump($_SESSION['game']);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Memory</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<div class="main-wrapper">
+    <!-- <form action="">
+        <button type="submit">Jouer</button>
+    </form> -->
+    
+    <form action="">
+        <div class="grid">
+            <?= isset($current_game) ? $current_game->toHtml() : '' ?>
+
+            <?php //var_dump($game->getCards()) ?>
+        </div>
+    </form>
+</div>
